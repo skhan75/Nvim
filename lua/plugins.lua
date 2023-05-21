f413b4a983155204a13c77a01432da07ec4d9d38
skhan75 <sami.ahmadkhan12@gmail.com>
2023-05-20 17:23:10 -0700

    removing unused plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -215,11 +215,6 @@ function M.setup()
     use {
       "hrsh7th/nvim-cmp",
       requires = {
-        -- "quangnguyen30192/cmp-nvim-ultisnips",
-        -- config = function()
-        --   -- optional call to setup (see customization section)
-        --   require("cmp_nvim_ultisnips").setup {}
-        -- end,
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
@@ -237,18 +232,6 @@ function M.setup()
         require("config.cmp").setup()
       end,
     }
-
-    -- use {
-    --   "SirVer/ultisnips",
-    --   requires = { { "honza/vim-snippets", rtp = "." }, "mlaursen/vim-react-snippets" },
-    --   config = function()
-    --     vim.g.UltiSnipsExpandTrigger = "<Plug>(ultisnips_expand)"
-    --     vim.g.UltiSnipsJumpForwardTrigger = "<Plug>(ultisnips_jump_forward)"
-    --     vim.g.UltiSnipsJumpBackwardTrigger = "<Plug>(ultisnips_jump_backward)"
-    --     vim.g.UltiSnipsListSnippets = "<c-x><c-s>"
-    --     vim.g.UltiSnipsRemoveSelectModeMappings = 0
-    --   end,
-    -- }
 
     use { 'morgsmccauley/vim-react-native-snippets' }
 
